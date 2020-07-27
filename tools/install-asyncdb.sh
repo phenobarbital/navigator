e8a53f19e6a8087aef0decd64b19fed8d202e652
phenobarbital <jesuslarag@gmail.com>
2020-07-27 04:10:44 +0200

    updating script for installation of asyncdb

--- tools/install-asyncdb.sh
+++ tools/install-asyncdb.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 sudo apt-get install libmemcached-dev zlib1g-dev
 # first: adding submodules
+mkdir extensions
+cd extensions
 git submodule add git@bitbucket.org:mobileinsight1/asyncdb.git
 git submodule init
 git submodule update --init --recursive --remote
+# installing
+pip install -e asyncdb
+# installing asyncdb requirements
+pip install -r asyncdb/requirements.txt

2559f05f23259610c1e2c9c4764845f35906f70e
phenobarbital <jesuslarag@gmail.com>
2020-09-08 01:30:13 +0200

    coloring messages

--- navigator/commands/__init__.py
+++ navigator/commands/__init__.py
@@ -9,6 +9,71 @@ from io import TextIOBase
 from navigator import get_version
 import traceback
 
+
+class colors:
+    """
+    Colors class.
+
+       reset all colors with colors.reset;
+       Use as colors.subclass.colorname.
+    i.e. colors.fg.red or colors.fg.greenalso, the generic bold, disable,
+    underline, reverse, strike through,
+    and invisible work with the main class i.e. colors.bold
+    """
+
+    reset = '\033[0m'
+    bold = '\033[01m'
+    disable = '\033[02m'
+    underline = '\033[04m'
+    reverse = '\033[07m'
+    strikethrough = '\033[09m'
+    invisible = '\033[08m'
+
+    class fg:
+        """
+        colors.fg.
+
+        Foreground Color subClass
+        """
+
+        black = '\033[30m'
+        red = '\033[31m'
+        green = '\033[32m'
+        orange = '\033[33m'
+        blue = '\033[34m'
+        purple = '\033[35m'
+        cyan = '\033[36m'
+        lightgrey = '\033[37m'
+        darkgrey = '\033[90m'
+        lightred = '\033[91m'
+        lightgreen = '\033[92m'
+        yellow = '\033[93m'
+        lightblue = '\033[94m'
+        pink = '\033[95m'
+        lightcyan = '\033[96m'
+
+def cPrint(msg, color=None, level='INFO'):
+    try:
+        if color:
+            coloring = colors.bold + getattr(colors.fg, color)
+        elif level:
+            if level == 'INFO':
+                coloring = colors.bold + colors.fg.green
+            elif level == 'DEBUG':
+                coloring = colors.fg.lightblue
+            elif level == 'WARN':
+                coloring = colors.bold + colors.fg.yellow
+            elif level == 'ERROR':
+                coloring = colors.fg.lightred
+            elif level == 'CRITICAL':
+                coloring = colors.bold + colors.fg.red
+            else:
+                coloring = colors.reset
+    except Exception as err:
+        print('Wrong color schema {}, error: {}'.format(color, str(err)))
+        coloring = colors.reset
+    print(coloring + msg, colors.reset)
+
 class CommandError(Exception):
     """
      Exception Base Class for raise problems in the execution of a Command
@@ -63,7 +128,7 @@ class BaseCommand(object):
             # parsing current arguments
             options = self.parser.parse_args(self.args)
             if options.debug:
-                print('Executing : {}'.format(self.action))
+                cPrint('Executing : {}'.format(self.action), level='DEBUG')
             # calling the internal function:
             if not hasattr(self, self.action):
                 raise CommandNotFound('Method {} from {} not Found'.format(__name__, self.action))
@@ -84,7 +149,7 @@ class BaseCommand(object):
                 print(traceback.format_exc())
             raise CommandError('Error Parsing arguments: {}'.format(err))
         finally:
-            print(output)
+            cPrint(output, 'INFO')
 
 def run_command(**kwargs):
     """

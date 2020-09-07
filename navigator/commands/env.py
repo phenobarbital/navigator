65bd96137d07d25e731c6f88a0f7426d0368f35d
phenobarbital <jesuslarag@gmail.com>
2020-09-08 01:43:30 +0200

    creation of dirs

--- navigator/commands/env.py
+++ navigator/commands/env.py
@@ -36,6 +36,7 @@ class EnvCommand(BaseCommand):
         create.
             Create a new Enviroment from scratch
         """
+        path = kwargs['project_path']
         output = ''
         OPTS = options
         if options.debug:
@@ -43,13 +44,13 @@ class EnvCommand(BaseCommand):
             cPrint('= wait a few minutes', level='WARN')
         #apps, etc, env, services, settings, static/images/js/css, templates
         cPrint('First Step: Creating Directory structure')
-        create_dir('apps')
-        create_dir('env/testing')
-        create_dir('etc')
-        create_dir('services')
-        create_dir('settings')
-        create_dir('static/images')
-        create_dir('static/js')
-        create_dir('static/css')
-        create_dir('templates')
+        create_dir(path, 'apps')
+        create_dir(path, 'env/testing')
+        create_dir(path, 'etc')
+        create_dir(path, 'services')
+        create_dir(path, 'settings')
+        create_dir(path, 'static/images')
+        create_dir(path, 'static/js')
+        create_dir(path, 'static/css')
+        create_dir(path, 'templates')
         return output

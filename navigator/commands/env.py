073fe75a82ad3e3b9b10cc12b653baae955aee47
phenobarbital <jesuslarag@gmail.com>
2020-09-08 01:42:07 +0200

    mandatory creation of directories

--- navigator/commands/env.py
+++ navigator/commands/env.py
@@ -1,4 +1,30 @@
 from . import BaseCommand, cPrint
+from pathlib import Path
+from aiofile import AIOFile
+import asyncio
+import logging
+
+logger = logging.getLogger('Navigator.creator')
+loop = asyncio.get_event_loop()
+OPTS = None
+
+def create_dir(dir, name):
+    try:
+        path = dir.joinpath(name)
+        if OPTS.debug:
+            cPrint('Creating Directory: {}'.format(path), color=lightcyan)
+        path.mkdir(parents=True, exist_ok=True)
+    except FileExistsError as exc:
+        pass
+
+def save_file(dir, filename, content):
+    async def main(filename, content):
+        path = dir.joinpath(Path(filename).resolve())
+        async with AIOFile(path, 'w+') as afp:
+            await afp.write(content)
+            await afp.fsync()
+        return True
+    return loop.run_until_complete(main(filename, content))
 
 class EnvCommand(BaseCommand):
     def parse_arguments(self):
@@ -11,7 +37,19 @@ class EnvCommand(BaseCommand):
             Create a new Enviroment from scratch
         """
         output = ''
+        OPTS = options
         if options.debug:
             cPrint(':: Creating a New Navigator Enviroment', level='INFO')
             cPrint('= wait a few minutes', level='WARN')
+        #apps, etc, env, services, settings, static/images/js/css, templates
+        cPrint('First Step: Creating Directory structure')
+        create_dir('apps')
+        create_dir('env/testing')
+        create_dir('etc')
+        create_dir('services')
+        create_dir('settings')
+        create_dir('static/images')
+        create_dir('static/js')
+        create_dir('static/css')
+        create_dir('templates')
         return output

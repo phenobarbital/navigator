b71aeb4f2ca9115e8fcf24ae8bc8860bfac65a80
phenobarbital <jesuslarag@gmail.com>
2020-09-08 01:33:35 +0200

    check if create works

--- navigator/commands/env.py
+++ navigator/commands/env.py
@@ -1,6 +1,15 @@
-from . import BaseCommand
+from . import BaseCommand, cPrint
 
 class EnvCommand(BaseCommand):
     def parse_arguments(self):
         self.parser.add_argument('--enable-notify')
         self.parser.add_argument('--process-services')
+
+    def create(self, options, **kwargs):
+        """
+        create.
+            Create a new Enviroment from scratch
+        """
+        if options.debug:
+            cPrint(':: Creating a New Navigator Enviroment', level='INFO')
+            cPrint('= wait a few minutes', level='WARN')

86095ed700a1213e10d20129670c23db59f948c6
phenobarbital <jesuslarag@gmail.com>
2020-09-08 00:40:59 +0200

    test cls

--- navigator/commands/__init__.py
+++ navigator/commands/__init__.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 import os
 import sys
+import importlib
 from typing import List, Any, Dict, Callable
 from argparse import ArgumentParser, HelpFormatter
 from io import TextIOBase
 import navigator
+
 
 class CommandError(Exception):
     """
@@ -12,6 +14,11 @@ class CommandError(Exception):
     """
     pass
 
+class CommandNotFound(Exception):
+    """
+     Exception Base Class for raise problems in the execution of a Command
+    """
+    pass
 
 class BaseCommand(object):
     parser: Callable
@@ -64,3 +71,9 @@ def run_command(**kwargs):
             # calling Command
             clsCommand = '{}Command'.format(command.capitalize())
             print('Command {}, cls: {}'.format(command, clsCommand))
+            try:
+                module = importlib.import_module(clsCommand, package='commands')
+                cls = getattr(module, clsCommand)
+                print(cls)
+            except ImportError:
+                raise CommandNotFound(message = "No Command %s was found" % clsCommand)

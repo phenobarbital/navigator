89bd71679993b9021d4e0685032f7f37a408d71a
phenobarbital <jesuslarag@gmail.com>
2020-09-08 02:15:26 +0200

    adding templates for all necessary files

--- navigator/commands/env.py
+++ navigator/commands/env.py
@@ -1,3 +1,4 @@
+import sys
 from . import BaseCommand, cPrint
 from pathlib import Path
 from aiofile import AIOFile
@@ -6,6 +7,19 @@ import logging
 
 logger = logging.getLogger('Navigator.creator')
 loop = asyncio.get_event_loop()
+
+
+def read_file(file):
+    path = Path(sys.prefix).resolve()
+    return open(path.joinpath('files', filename), 'r')
+
+env = read_file('env.tpl')
+ini = read_file('ini.tpl')
+settings = read_file('settings.tpl')
+localsettings = read_file('localsettings.tpl')
+
+run = read_file('run.tpl')
+app = read_file('app.tpl')
 
 def create_dir(dir, name):
     try:
@@ -16,11 +30,14 @@ def create_dir(dir, name):
 
 def save_file(dir, filename, content):
     async def main(filename, content):
-        path = dir.joinpath(Path(filename).resolve())
-        async with AIOFile(path, 'w+') as afp:
-            await afp.write(content)
-            await afp.fsync()
-        return True
+        if content:
+            path = dir.joinpath(Path(filename).resolve())
+            async with AIOFile(path, 'w+') as afp:
+                await afp.write(content)
+                await afp.fsync()
+            return True
+        else:
+            return False
     return loop.run_until_complete(main(filename, content))
 
 class EnvCommand(BaseCommand):
@@ -50,114 +67,19 @@ class EnvCommand(BaseCommand):
         create_dir(path, 'static/css')
         create_dir(path, 'templates')
         cPrint('* Second Step: Creation of Empty .env File')
-        env ="""[general]
-        CONFIG_FILE=etc/navigator.ini
-
-        [api]
-        API_HOST=nav-api.dev.local:5000
-
-        [cache]
-        CACHEHOST=127.0.0.1
-        CACHEPORT=6379
-        QUERYSET_DB=0
-        MEMCACHE_HOST=127.0.0.1
-        MEMCACHE_PORT=11211
-        CACHE_PREFIX=local
-
-        [debug]
-        PRODUCTION=false
-        DEBUG=true
-        """
         save_file(path, 'env/.env', env)
         save_file(path, 'env/testing/.env', env)
-
-        ini = """# basic information
-        [info]
-        OWNER: TROC
-        APP_NAME: Navigator by Mobile Insight
-        APP_TITLE: Navigator
-        EMAIL_CONTACT: jlara@example.com
-
-        [ssl]
-        SSL: false
-        CERT: /etc/ssl/certs/example.com.crt
-        KEY: /etc/ssl/certs/example.com.key
-
-        [logging]
-        logdir: /var/log/navigator
-        """
         save_file(path, 'etc/navigator.ini', ini)
         cPrint('* Third Step: Creation of Empty settings.py File')
-        settings = """# -*- coding: utf-8 -*-
-        #!/usr/bin/env python3
-        import os
-        import sys
-        from pathlib import Path
-        import logging
-        #from navigator.conf import config, DEBUG
-        from navigator.navigatorConfig import config, BASE_DIR
-
-        # Debug
-        DEBUG = config.getboolean('DEBUG', fallback=True)
-        LOCAL_DEVELOPMENT = (DEBUG == True and sys.argv[0] == 'run.py')"""
         save_file(path, 'settings/settings.py', settings)
-
-        local = """# -*- coding: utf-8 -*-
-        import os
-        import sys
-        from navigator.navigatorConfig import config, BASE_DIR
-
-        '''
-        Example Local Settings
-        '''
-        # EXAMPLE = config.get('EXAMPLE_KEY')"""
-        save_file(path, 'settings/local_settings.py.example', local)
+        save_file(path, 'settings/local_settings.py.example', localsettings)
         cPrint('* Fourt Step: Creation of a run.py File')
-        run = """#!/usr/bin/env python3
-        from navigator import Application
-        import asyncio
-
-        # define new Application
-        app = Application()
-
-        if __name__ == '__main__':
-            app.run()
-        """
         save_file(path, 'run.py', run)
-
+        save_file(path, 'app.py', app)
         cPrint('* Fifth Step: adding a *home* template')
-        home = """
-        <!doctype html>
-
-        <html lang="en">
-        <head>
-          <meta charset="utf-8">
-
-          <title>Navigator API</title>
-          <meta name="description" content="Navigator API">
-          <meta name="author" content="Navigator API">
-          <link rel="stylesheet" href="static/css/styles.css?v=1.0">
-
-        </head>
-
-        <body>
-          <script src="static/js/scripts.js"></script>
-          <h1>Welcome to Navigator</h1>
-          <p>This is Navigator, a "batteries-included" Framework, a comprehensive collection of asyncio-based libraries to easily create any python project in minutes!
-          </p>
-          <p>
-            Navigator is based on python >= 3.8, Asyncio and other fantastic asynchronous technologies like:
-            <ul>
-              <li> Aiohttp </li>
-              <li> Python 3.8 </li>
-              <li> Asyncio+uvloop </li>
-              <li> asyncpg </li>
-              <li> pydantic </li>
-              <li> SockJS </li>
-            </ul>
-            And many others, designed with "functional-first" philosofy, is an async "micro-django" with many tools for easy deploy and development async applications.
-        </body>
-        </html>
-        """
+        home = read_file('home.html')
+        css = read_file('styles.css')
         save_file(path, 'templates/home.html', home)
+        save_file(path, 'static/css/styles.css', css)
+        save_file(path, 'static/js/scripts.js', js)
         return output
---
+++ navigator/commands/templates/env.tpl
@@ -0,0 +1,17 @@
+[general]
+CONFIG_FILE=etc/navigator.ini
+
+[api]
+API_HOST=nav-api.dev.local:5000
+
+[cache]
+CACHEHOST=127.0.0.1
+CACHEPORT=6379
+QUERYSET_DB=0
+MEMCACHE_HOST=127.0.0.1
+MEMCACHE_PORT=11211
+CACHE_PREFIX=local
+
+[debug]
+PRODUCTION=false
+DEBUG=true

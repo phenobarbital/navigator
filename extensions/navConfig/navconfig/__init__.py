279486b707fb6e2cd756da18c8d9d0a4303451a7
phenobarbital <jesuslarag@gmail.com>
2020-10-16 20:10:18 +0200

    migration of logging config

--- extensions/navConfig/navconfig/__init__.py
+++ extensions/navConfig/navconfig/__init__.py
@@ -2,8 +2,6 @@ import sys
 import os
 from pathlib import Path
 from .config import navigatorConfig
-#__all__ = (navigatorConfig)
-
 
 def is_virtualenv():
     return (hasattr(sys, 'real_prefix') or (hasattr(sys, 'base_prefix') and sys.base_prefix != sys.prefix))
@@ -35,20 +33,51 @@ sys.path.append(str(SETTINGS_DIR))
 sys.path.append(str(EXTENSION_DIR))
 
 from .conf import *
-#
-# # get settings
-# try:
-#     from settings.settings import *
-# except ImportError:
-#     try:
-#         from settings import *
-#     except ImportError:
-#         print('Its recommended to use a settings module inside to customize a Service Configuration')
-#
-# """
-# User Local Settings
-# """
-# try:
-#     from settings.local_settings import *
-# except ImportError:
-#     pass
+
+"""
+Logging
+"""
+logdir = config.get('logdir', section='logging', fallback='/tmp')
+if DEBUG:
+    loglevel = logging.DEBUG
+else:
+    loglevel = logging.INFO
+
+logging_config = dict(
+    version = 1,
+    formatters = {
+        "console": {
+            'format': '%(message)s'
+        },
+        "file": {
+            "format": "%(asctime)s: [%(levelname)s]: %(pathname)s: %(lineno)d: \n%(message)s\n"
+        },
+        'default': {
+            'format': '[%(levelname)s] %(asctime)s %(name)s: %(message)s'}
+        },
+    handlers = {
+        "console": {
+                "formatter": "console",
+                "class": "logging.StreamHandler",
+                "stream": "ext://sys.stdout",
+                'level': loglevel
+        },
+        'StreamHandler': {
+                'class': 'logging.StreamHandler',
+                'formatter': 'default',
+                'level': loglevel
+        },
+        'RotatingFileHandler': {
+                'class': 'logging.handlers.RotatingFileHandler',
+                'filename': '{0}/{1}.log'.format(logdir, 'query_api'),
+                'maxBytes': (1048576*5),
+                'backupCount': 2,
+                'encoding': 'utf-8',
+                'formatter': 'file',
+                'level': loglevel}
+        },
+    root = {
+        'handlers': ['StreamHandler','RotatingFileHandler'],
+        'level': loglevel,
+        },
+)
--- navigator/conf.py
+++ navigator/conf.py
@@ -86,67 +86,8 @@ Basic Information
 EMAIL_CONTACT = config.get('EMAIL_CONTACT',section='info', fallback='foo@example.com')
 API_NAME = config.get('API_NAME',section='info', fallback='Navigator')
 
-"""
-Logging
-"""
-logdir = config.get('logdir', section='logging', fallback='/tmp')
-if DEBUG:
-    loglevel = logging.DEBUG
-else:
-    loglevel = logging.INFO
-
-logging_config = dict(
-    version = 1,
-    formatters = {
-        "console": {
-            'format': '%(message)s'
-        },
-        "file": {
-            "format": "%(asctime)s: [%(levelname)s]: %(pathname)s: %(lineno)d: \n%(message)s\n"
-        },
-        'default': {
-            'format': '[%(levelname)s] %(asctime)s %(name)s: %(message)s'}
-        },
-    handlers = {
-        "console": {
-                "formatter": "console",
-                "class": "logging.StreamHandler",
-                "stream": "ext://sys.stdout",
-                'level': loglevel
-        },
-        'StreamHandler': {
-                'class': 'logging.StreamHandler',
-                'formatter': 'default',
-                'level': loglevel
-        },
-        'RotatingFileHandler': {
-                'class': 'logging.handlers.RotatingFileHandler',
-                'filename': '{0}/{1}.log'.format(logdir, 'query_api'),
-                'maxBytes': (1048576*5),
-                'backupCount': 2,
-                'encoding': 'utf-8',
-                'formatter': 'file',
-                'level': loglevel}
-        },
-    root = {
-        'handlers': ['StreamHandler','RotatingFileHandler'],
-        'level': loglevel,
-        },
-)
-
 # get settings
-try:
-    from settings.settings import *
-except ImportError:
-    print('Its recommended to use a settings/settings module to customize Navigator Configuration')
-
-"""
-User Local Settings
-"""
-try:
-    from settings.local_settings import *
-except ImportError:
-    pass
+from navconfig.conf import *
 
 #######################
 ##

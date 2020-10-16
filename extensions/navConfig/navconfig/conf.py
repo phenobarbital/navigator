e590116f11935e92f74e732d0b184b6bbf6cc3af
phenobarbital <jesuslarag@gmail.com>
2020-10-16 20:12:00 +0200

    tryit config

--- extensions/navConfig/navconfig/conf.py
+++ extensions/navConfig/navconfig/conf.py
@@ -11,3 +11,51 @@ try:
     from settings.local_settings import *
 except ImportError:
     pass
+
+"""
+Logging
+"""
+logdir = config.get('logdir', section='logging', fallback='/tmp')
+if DEBUG:
+    loglevel = logging.DEBUG
+else:
+    loglevel = logging.INFO
+
+logging_config = dict(
+    version = 1,
+    formatters = {
+        "console": {
+            'format': '%(message)s'
+        },
+        "file": {
+            "format": "%(asctime)s: [%(levelname)s]: %(pathname)s: %(lineno)d: \n%(message)s\n"
+        },
+        'default': {
+            'format': '[%(levelname)s] %(asctime)s %(name)s: %(message)s'}
+        },
+    handlers = {
+        "console": {
+                "formatter": "console",
+                "class": "logging.StreamHandler",
+                "stream": "ext://sys.stdout",
+                'level': loglevel
+        },
+        'StreamHandler': {
+                'class': 'logging.StreamHandler',
+                'formatter': 'default',
+                'level': loglevel
+        },
+        'RotatingFileHandler': {
+                'class': 'logging.handlers.RotatingFileHandler',
+                'filename': '{0}/{1}.log'.format(logdir, 'query_api'),
+                'maxBytes': (1048576*5),
+                'backupCount': 2,
+                'encoding': 'utf-8',
+                'formatter': 'file',
+                'level': loglevel}
+        },
+    root = {
+        'handlers': ['StreamHandler','RotatingFileHandler'],
+        'level': loglevel,
+        },
+)

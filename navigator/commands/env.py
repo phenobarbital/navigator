2c5442c430b5858610959ff653417e1dc369e825
phenobarbital <jesuslarag@gmail.com>
2021-01-06 13:29:58 +0100

    check for save_file functionality on command env

--- navigator/commands/env.py
+++ navigator/commands/env.py
@@ -30,10 +30,7 @@ def create_dir(dir, name, touch_init: bool = False):
 def save_file(dir, filename, content):
     async def main(filename, content):
         try:
-            f = Path(filename).resolve()
-            print(f)
             path = dir.joinpath(filename)
-            print(path)
             async with AIOFile(path, "w+") as afp:
                 await afp.write(content)
                 await afp.fsync()

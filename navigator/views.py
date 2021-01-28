777c60cce65ee2585eacca3ce9f69ae5acdc8597
Jesus Lara <jesuslarag@gmail.com>
2021-01-28 02:54:30 +0100

    adding ModelView

--- navigator/views.py
+++ navigator/views.py
@@ -485,6 +485,19 @@ class DataView(BaseView):
 
 
 class ModelView(BaseView):
+    """ModelView.
+
+    description: API View using AsyncDB Models.
+    tags:
+      - Model
+      - AsyncDB Model
+    parameters:
+      - name: model
+        in: Model
+        type: Model
+        required: true
+        description: DB Model using asyncdb Model.
+    """
     model: Model = None
     models: list = []
 
@@ -499,6 +512,7 @@ class ModelView(BaseView):
             except Exception as err:
                 print(err)
                 table = type(self).__name__
+                self.Meta.tablename = table
             try:
                 return self.models[table]
             except KeyError:
@@ -515,11 +529,11 @@ class ModelView(BaseView):
 
     async def get_data(self, params, args):
         try:
-            if len(args) > 0:
-                query = await self.model.get(**args)
-                return query.dict()
             if len(params) > 0:
                 query = await self.model.filter(**params)
+            elif len(args) > 0:
+                query = await self.model.get(**args)
+                return query.dict()
             else:
                 query = await self.model.all()
             if query:
@@ -537,17 +551,35 @@ class ModelView(BaseView):
             headers=headers
         )
 
-    def get_parameters(self):
+    def get_args(self, request: web.Request = None) -> dict:
+        params = {}
+        if not request:
+            rq = self.request
+        else:
+            rq = request
+        for arg in rq.match_info:
+            try:
+                val = rq.match_info.get(arg)
+                object.__setattr__(self, arg, val)
+                params[arg] = val
+            except AttributeError:
+                pass
+        return params
+
+    async def get_parameters(self):
         """Get Parameters.
 
         Get all parameters from URL or from query string.
         """
-        args = self.get_arguments()
+        if not self.model.Meta.connection:
+            db = await self.request.app['database'].acquire()
+            self.model.Meta.connection = db
+        args = self.get_args()
         params = self.query_parameters(self.request)
         return [args, params]
 
     async def get(self):
-        args, params = self.get_parameters()
+        args, params = await self.get_parameters()
         # TODO: check if QueryParameters are in list of columns in Model
         try:
             data = await self.get_data(params, args)
@@ -556,7 +588,7 @@ class ModelView(BaseView):
             print(err)
             headers = {
                     'X-STATUS': 'EMPTY',
-                    'X-MESSAGE': f'Data on {self.tablename} not Found'
+                    'X-MESSAGE': f'Data on {self.Meta.tablename} not Found'
                 }
             return self.no_content(headers=headers)
         except Exception as err:
@@ -573,12 +605,12 @@ class ModelView(BaseView):
             summary: return the metadata from table or, if we got post
             realizes a partially atomic updated of the query.
         """
-        args, params = self.get_parameters()
+        args, params = await self.get_parameters()
         # try to got post data
         post = await self.json_data()
         if post:
             # trying to update the model
-            update = await self.model.update(params, **post)
+            update = await self.model.update(args, **post)
             if update:
                 data = update[0].dict()
                 return self.model_response(data)
@@ -591,7 +623,7 @@ class ModelView(BaseView):
         else:
             try:
                 # getting metadata of Model
-                qry = self.model(**params)
+                qry = self.model(**args)
                 data = {}
                 for name, field in qry.columns().items():
                     key = field.name
@@ -613,7 +645,7 @@ class ModelView(BaseView):
         post.
             summary: update (or create) a row in table
         """
-        args, params = self.get_parameters()
+        args, params = await self.get_parameters()
         post = await self.json_data()
         if not post:
             return self.error(
@@ -622,18 +654,32 @@ class ModelView(BaseView):
                 exception=err,
                 state=406
             )
-        parameters = {**params, **post}
+        if len(args) > 0:
+            parameters = {**args, **post}
+            try:
+                # check if exists first:
+                query = await self.model.get(**args)
+                if not query:
+                    # object doesnt exists, need to be created:
+                    result = await self.model.create([parameters])
+                    query = await self.model.get(**parameters)
+                    data = query.dict()
+                    return self.model_response(data)
+            except Exception as err:
+                print(err)
+                pass
+        # I need to use post data only
         try:
-            qry = self.model(**parameters)
+            qry = self.model(**post)
             if qry.is_valid():
                 await qry.save()
-                query = await self.model.get(**params)
+                query = await qry.fetch(**args)
                 data = query.dict()
                 return self.model_response(data)
             else:
                 return self.error(
                     request=self.request,
-                    response=f'Invalid data for Schema {self.tablename}'
+                    response=f'Invalid data for Schema {self.Meta.tablename}'
                 )
         except Exception as err:
             return self.critical(
@@ -647,52 +693,43 @@ class ModelView(BaseView):
         delete.
            summary: delete a table object
         """
-        args, params = self.get_parameters()
-        if len(params) > 0:
-            qry = self.model(**params)
-            if qry:
-                result = None
-                try:
-                    result = await qry.delete()
-                except Exception as err:
-                    return self.critical(
-                        request=self.request,
-                        exception=err,
-                        traceback=''
-                    )
-                if result:
-                    msg = {
-                        "result": result
-                    }
-                    headers = {
-                        'X-STATUS': 'OK',
-                        'X-MESSAGE': f'Table row with {params!s} was deleted',
-                        'X-TABLE': self.tablename
-                    }
-                    return self.model_response(
-                        msg,
-                        headers=headers
-                    )
-                else:
-                    headers = {
-                            'X-STATUS': 'Error',
-                            'X-MESSAGE': f'Row in Table {self.tablename} not deleted'
-                        }
-                    return self.error(
-                        response=f'Row in Table {self.tablename} not deleted',
-                        headers=headers,
-                        state=404
-                    )
+        args, params = await self.get_parameters()
+        try:
+            result = None
+            if len(args) > 0:
+                # need to delete one
+                result = await self.model.remove(args)
+            elif len(params) > 0:
+                result = await self.model.remove(params)
+        except Exception as err:
+            return self.error(
+                request=self.request,
+                response='Error Deleting Object',
+                exception=err,
+                state=400
+            )
+        if result is not None:
+            msg = {
+                "result": result
+            }
+            headers = {
+                'X-STATUS': 'OK',
+                'X-MESSAGE': f'Table row was deleted',
+                'X-TABLE': self.Meta.tablename
+            }
+            return self.model_response(
+                msg,
+                headers=headers
+            )
         else:
             headers = {
                     'X-STATUS': 'Error',
-                    'X-MESSAGE': f'Parameter for DELETE are missing'
+                    'X-MESSAGE': f'Row in Table {self.Meta.tablename} not deleted'
             }
             return self.error(
-                request=self.request,
-                response=f"Missing Parameters for DELETE on {self.tablename}",
-                state=403,
-                headers=headers
+                response=f'Row in Table {self.Meta.tablename} not deleted',
+                headers=headers,
+                state=404
             )
 
     async def put(self):
@@ -700,7 +737,7 @@ class ModelView(BaseView):
         put.
            summary: insert a row in table
         """
-        args, params = self.get_parameters()
+        args, params = await self.get_parameters()
         post = await self.json_data()
         if not post:
             return self.error(
@@ -721,7 +758,7 @@ class ModelView(BaseView):
             else:
                 return self.error(
                     request=self.request,
-                    response=f'Invalid data for Schema {self.tablename}'
+                    response=f'Invalid data for Schema {self.Meta.tablename}'
                 )
         except Exception as err:
             return self.critical(

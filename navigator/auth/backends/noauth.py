0c5201adf8e5508f0ff75604bebb873cf58bf6ef
Jesus Lara <jesuslarag@gmail.com>
2022-02-01 23:44:21 +0100

    WiP: NoAuth Authentication

--- navigator/auth/backends/noauth.py
+++ navigator/auth/backends/noauth.py
@@ -12,7 +12,8 @@ from navigator.auth.sessions import get_session
 from navigator.exceptions import (
     NavException,
     FailedAuth,
-    InvalidAuth
+    InvalidAuth,
+    NAV_SESSION_OBJECT
 )
 
 class NoAuth(BaseAuthBackend):
@@ -24,18 +25,32 @@ class NoAuth(BaseAuthBackend):
         """ Authentication and create a session."""
         return True
 
-    async def authenticate(self, request):
+    def get_userdata(self):
         key = uuid.uuid4().hex
+        userdata = {
+            NAV_SESSION_OBJECT: {
+                "session": key,
+                self.username_attribute: "Anonymous",
+                "first_name": "Anonymous",
+                "last_name": "User"
+            }
+        }
+        return [ userdata, key ]
+
+    async def authenticate(self, request):
+        userdata, key = self.get_userdata()
         payload = {
             self.session_key_property: key,
             self.user_property: None,
-            self.username_attribute: "Anonymous"
+            self.username_attribute: "Anonymous",
+            **userdata
         }
         token = self.create_jwt(data=payload)
         return {
             "token": token,
             self.session_key_property: key,
-            self.username_attribute: "Anonymous"
+            self.username_attribute: "Anonymous",
+            **userdata
         }
 
     async def auth_middleware(self, app, handler):

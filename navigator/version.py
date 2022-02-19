15c1be2e5c9715bfb1f59dd91ae7b923cde2f9f4
Jesus Lara <jesuslarag@gmail.com>
2022-02-19 02:47:44 +0100

    name Navigator.

--- navigator/version.py
+++ navigator/version.py
@@ -1,6 +1,6 @@
 """Navigator Meta information."""
 
-__title__ = 'navigator-api'
+__title__ = 'navigator'
 __description__ = ('Navigator Web Framework based on aiohttp, '
                    'with batteries included.')
 __version__ = '2.0.0'

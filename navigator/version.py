2656f1c82a68b94e1b4726796829efad98fed2b2
Jesus Lara <jesuslarag@gmail.com>
2022-03-03 19:43:24 +0100

    version-next

--- navigator/version.py
+++ navigator/version.py
@@ -3,7 +3,7 @@
 __title__ = 'navigator'
 __description__ = ('Navigator Web Framework based on aiohttp, '
                    'with batteries included.')
-__version__ = '2.0.3'
+__version__ = '2.0.4'
 __author__ = 'Jesus Lara'
 __author_email__ = 'jesuslarag@gmail.com'
 __license__ = 'BSD'

69b35a8ae1ba1ea3630cb43d78a82ed300cb046e
Jesus Lara <jesuslarag@gmail.com>
2022-03-03 23:24:17 +0100

    bump version

--- navigator/version.py
+++ navigator/version.py
@@ -3,7 +3,7 @@
 __title__ = 'navigator'
 __description__ = ('Navigator Web Framework based on aiohttp, '
                    'with batteries included.')
-__version__ = '2.0.5'
+__version__ = '2.0.6'
 __author__ = 'Jesus Lara'
 __author_email__ = 'jesuslarag@gmail.com'
 __license__ = 'BSD'

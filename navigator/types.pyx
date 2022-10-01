cd734a948d578248af4e659c93e4ba15f0b1f0d2
Jesus Lara <jesuslarag@gmail.com>
2022-10-01 04:07:56 +0200

    fix URL type

--- navigator/types.pyx
+++ navigator/types.pyx
@@ -1,8 +1,6 @@
 # cython: language_level=3, embedsignature=True, boundscheck=False, wraparound=True, initializedcheck=False
 # Copyright (C) 2018-present Jesus Lara
 #
-import httptools
-from httptools.parser import errors
 from libcpp cimport bool
 
 from typing import Tuple, Callable, Awaitable

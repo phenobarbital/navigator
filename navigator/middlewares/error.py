d76c531d6adac0c817bb37206965d1474d58369d
Jesus Lara <jesuslarag@gmail.com>
2022-10-13 00:30:30 +0200

    fix error middleware

--- navigator/middlewares/error.py
+++ navigator/middlewares/error.py
@@ -9,7 +9,25 @@ from navconfig.logging import logging
 from navigator.responses import HTMLResponse, JSONResponse
 
 
-error_codes = (400, 404, 500, 501, 502, 503)
+error_codes = (400, 500, 501, 502, 503)
+
+not_found = """
+<h1>{name}</h1>
+<div id="main">
+    <div>
+        <h1>Error {status}</h1>
+        <h3>{error}</h3>
+        <h4>URL: {url}</h4>
+    </div>
+</div>
+<div>
+<p>{message}</p>
+<h4>Stacktrace: </h4>
+<div><pre>
+{stacktrace}</pre>
+</div>
+</div>
+"""
 
 
 error_page = """
@@ -29,9 +47,44 @@ error_page = """
 </div>
 """
 
+
+def manage_notfound(app: web.Application, request: web.Request, response: web.Response = None, ex: BaseException = None, status: int = None) -> web.Response:
+    name = app['name']
+    if response:
+        message = response.reason
+        status = response.status
+        ct = response.content_type
+    else:
+        message = None
+        ct = 'text/html'
+    if ex is not None:
+        error = ex.__class__.__name__
+        detail = str(ex)
+        stacktrace = traceback.format_exc(limit=20, chain=True)
+    else:
+        error = None
+        detail = message
+        stacktrace = None
+    payload = {
+        "name": name,
+        "status": status,
+        "url": request.rel_url,
+        "error": error,
+        "message": detail,
+        "stacktrace": stacktrace
+    }
+    if ct == 'application/json':
+        return JSONResponse(payload, status=status)
+    else:
+        data = not_found.format(**payload)
+        return HTMLResponse(content=data, status=status)
+
+
 def manage_exception(app: web.Application, response: web.Response = None, ex: BaseException = None, status: int = None) -> web.Response:
     if 'template' in app:
         use_template = True
+    else:
+        use_template = False
     name = app['name']
     if response:
         if isinstance(response, Exception):
@@ -65,7 +118,6 @@ def manage_exception(app: web.Application, response: web.Response = None, ex: Ba
             pass
         else:
             data = error_page.format(**payload)
-            print(data)
             return HTMLResponse(content=data, status=status)
 
 
@@ -83,7 +135,10 @@ async def error_middleware(
                 else:
                     return response
         except web.HTTPException as ex:
-            if ex.status in error_codes:
+            if ex.status == 404:
+                if DEBUG is True:
+                    return manage_notfound(app, request=request, status=ex.status, ex=ex)
+            elif ex.status in error_codes:
                 if DEBUG is True:
                     return manage_exception(app, status=ex.status, ex=ex)
                 else:
@@ -91,8 +146,8 @@ async def error_middleware(
             else:
                 raise
         except asyncio.CancelledError:
-            raise
-        except Exception as ex:
+            pass
+        except Exception as ex: # pylint: disable=W0703
             logging.warning(f'Request {request} has failed with exception: {ex!r}')
             if DEBUG is True:
                     return manage_exception(app, status=ex, ex=ex)

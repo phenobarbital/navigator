3dc10c9aa05f475d7694e7cc26e3fd356f7d30fd
Jesus Lara <jesuslarag@gmail.com>
2022-11-28 19:52:19 +0100

    removed settings from code

--- navigator/version.py
+++ navigator/version.py
@@ -3,7 +3,7 @@
 __title__ = 'navigator-api'
 __description__ = ('Navigator Web Framework based on aiohttp, '
                    'with batteries included.')
-__version__ = '2.3.41'
+__version__ = '2.3.42'
 __author__ = 'Jesus Lara'
 __author_email__ = 'jesuslarag@gmail.com'
 __license__ = 'BSD'

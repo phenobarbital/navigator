57f02e3d1c9acf92994b065c823f9a08d3756eac
Jesus Lara <jesuslarag@gmail.com>
2024-02-21 17:23:18 +0100

    removing aiofiles from dependencies (conflict with asyncdb)

--- setup.py
+++ setup.py
@@ -159,7 +159,6 @@ setup(
         "navconfig[default]>=1.5.0",
         "aiohttp-cors",
         "proxylists>=0.12.3",
-        "aiofiles==22.1.0",
         "httpx==0.26.0",
         "beautifulsoup4==4.12.2"
     ],

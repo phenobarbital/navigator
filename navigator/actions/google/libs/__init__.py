35ac0fa2a843422a80715c7ec959ecb3d1245e7c
Jesus Lara <jesuslarag@gmail.com>
2025-05-30 01:46:14 +0200

    migrated to new version of Google Travel Search

--- navigator/actions/google/libs/__init__.py
+++ navigator/actions/google/libs/__init__.py
@@ -10,3 +10,13 @@ class GoogleService(ABC):
             raise ValueError(
                 "Google API Key is not present."
             )
+
+    async def __aenter__(self):
+        self._logger.debug(f"Initializing {self.__class__.__name__} with API Key.")
+        return self
+
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        self._logger.debug(f"Exiting {self.__class__.__name__}.")
+        if exc_type:
+            self._logger.error(f"An error occurred: {exc_val}")
+        return False
